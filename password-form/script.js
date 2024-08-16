f56de24f3d3bc0f2ace0648deeddee2604dc6abd
hvson03 <hvson2003@gmail.com>
2024-08-16 21:25:49 +0700

    finished password form

---
+++ password-form/script.js
@@ -0,0 +1,12 @@
+const $eyeIcon =  document.getElementById("eye-icon");
+const $password =  document.getElementById("password");
+
+$eyeIcon.onclick = function() {
+    if ($password.type == "password") {
+        $password.type = "text";
+        $eyeIcon.src = "images/eye-open.png";
+    } else {
+        $password.type = "password";
+        $eyeIcon.src = "images/eye-close.png";
+    }
+}
